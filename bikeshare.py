c0ace1092640600ef0200a2334cf672819113090
Thiago <thiagoalmeidamacedo@gmail.com>
2021-04-18 16:06:23 -0300

    bug fix at 'main(df)' function

--- bikeshare.py
+++ bikeshare.py
@@ -182,6 +182,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df, city)
+        raw_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
